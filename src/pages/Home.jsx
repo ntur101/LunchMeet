951318b9066dd3bff5e0ba27d2726cebfd89b158
Kieran McKenna <kmck133@aucklanduni.ac.nz>
2025-08-02 16:32:19 +1200

    Add search bar to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,9 @@
+import { Input } from "/components/ui/input"
+
 function Home() {
-  return <div className="p-4">Dashboard Page - List of food items</div>;
+  return <div className="p-4">
+    Dashboard Page - List of food items
+    <Input type="" placeholder="Search for Food" className="bg-black"/>
+    </div>;
 }
 export default Home;
