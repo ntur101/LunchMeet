4bc214766e65c78189b3e8d4bef9740597901e6b
Kieran McKenna <kmck133@aucklanduni.ac.nz>
2025-08-02 19:55:35 +1200

    Populated home page

---
+++ components/ui/food-card.jsx
@@ -0,0 +1,17 @@
+import { Button } from "/components/ui/button"
+import { Link } from "react-router-dom"
+
+export function FoodCard({ imageSrc, label, to = "#", onClick }) {
+  return (
+    <Button asChild variant="ghost" className="p-0 rounded-xl overflow-hidden bg-[#C5BAFF] h-34 w-42 flex-shrink-0 shadow">
+      <Link to={to} onClick={onClick} className="flex flex-col items-center">
+        <img
+          src={imageSrc}
+          alt={label}
+          className="w-full h-24 object-cover"
+        />
+        <div className="p-2 font-semibold text-black text-center">{label}</div>
+      </Link>
+    </Button>
+  )
+}
