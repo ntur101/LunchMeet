04a863fb062be2fec2cde5d3dd6e4e01c1777c5d
Kieran McKenna <kmck133@aucklanduni.ac.nz>
2025-08-03 14:23:51 +1200

    a couple ui tweaks

--- components/ui/food-card.jsx
+++ components/ui/food-card.jsx
@@ -3,14 +3,18 @@ import { Link } from "react-router-dom"
 
 export function FoodCard({ imageSrc, label, id, onClick }) {
   return (
-    <Button asChild variant="ghost" className="p-0 rounded-xl overflow-hidden bg-[#C5BAFF] h-34 w-42 flex-shrink-0 shadow">
+    <Button
+        asChild
+        variant="ghost"
+        className="p-0 rounded-xl overflow-hidden bg-[#C5BAFF] h-30 w-42 flex-shrink-0"
+    >
       <Link to={`/food/${id}`} onClick={onClick} className="flex flex-col items-center">
         <img
           src={imageSrc}
           alt={label}
           className="w-full h-24 object-cover"
         />
-        <div className="p-2 font-semibold text-black text-center">{label}</div>
+        <div className="mb-6 font-semibold text-black text-center">{label}</div>
       </Link>
     </Button>
   )
